19fa746a27fbf659a299afec494eefa7f3c6a2c4
neights <neights@126.com>
2019-12-28 16:10:59 +0800

    ps emu chs

---
+++ make_opk.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+OPK_NAME=pcsx4all-psi.opk
+
+echo ${OPK_NAME}
+
+# create default.gcw0.desktop
+cat > default.gcw0.desktop <<EOF
+[Desktop Entry]
+Name=Pcsx4all
+Comment=Sony PSX emulator
+Exec=pcsx4all -iso %f
+Terminal=false
+Type=Application
+StartupNotify=true
+Icon=pcsxr-icon-small
+Categories=emulators;
+X-OD-NeedsDownscaling=true
+EOF
+
+# create opk
+FLIST="pcsx4all"
+FLIST="${FLIST} default.gcw0.desktop"
+FLIST="${FLIST} src/port/sdl/pcsxr-icon-small.png"
+if [ -f translations/locales ]; then
+  FLIST="${FLIST} translations/locales"
+  if [ -d locale ]; then
+    FLIST="${FLIST} locale"
+  fi
+fi
+
+rm -f ${OPK_NAME}
+mksquashfs ${FLIST} ${OPK_NAME} -all-root -no-xattrs -noappend -no-exports
+
+cat default.gcw0.desktop
+rm -f default.gcw0.desktop
